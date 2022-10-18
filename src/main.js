b6246c36d620a53a9c58f5dd2c7545838c33c1c3
Mathias Oxholm <mo.micheelsen@gmail.com>
2022-10-18 20:15:29 +0200

    Minor updates

--- src/main.js
+++ src/main.js
@@ -11,12 +11,36 @@ scroll.on('hs:complete', () => {
   console.log('HumbleScroll is initialized')
 })
 
-// function myFunction() {
-//   console.log('HumbleScroll')
-//   const card = document.querySelector('#callback-card-1')
-//   const cardHeading = document.querySelector('#callback-1-heading')
-//   card.style = 'background-color: purple; border-radius: 100%; transform: scale(0.75);'
-//   setTimeout(() => {
-//     cardHeading.innerHTML = 'Kinda cool right?'
-//   }, 4000)
-// }
+function myFunction() {
+  const card = document.querySelector('#callback-card-1')
+  const cardHeading = document.querySelector('#callback-card-1 h3')
+  card.style = 'background-color: purple; border-radius: 50px; transform: scale(0.85);'
+  cardHeading.style = 'transition: all 4000ms ease-out 0s; transform: scale(1.5) translate(120px, 72px);'
+
+  setTimeout(() => {
+    cardHeading.innerHTML = 'Kinda cool right?'
+  }, 4000)
+}
+
+/* window.addEventListener('scroll', throttle(parallax, 14))
+
+function throttle(fn, wait) {
+  var time = Date.now()
+  return function () {
+    if (time + wait - Date.now() < 0) {
+      fn()
+      time = Date.now()
+    }
+  }
+}
+
+function parallax() {
+  let scrolled = window.scrollY
+  let parallaxEl = document.querySelector('.parallax')
+  const parallaxRect = parallaxEl.getBoundingClientRect()
+  console.log(parallaxRect)
+  var coords = scrolled * 0.3 + 'px'
+  parallaxEl.style.transform = 'translateY(-' + coords + ')'
+}
+
+console.log(window) */
